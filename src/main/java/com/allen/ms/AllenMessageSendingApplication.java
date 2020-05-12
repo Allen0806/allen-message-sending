5dbf0b883cd66639eacafec50ec4334042218979
luoxuetong <luoxuetong@9fbank.com.cn>
2020-05-12 14:20:49 +0800

    初始提交

---
+++ src/main/java/com/allen/ms/AllenMessageSendingApplication.java
@@ -0,0 +1,13 @@
+package com.allen.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllenMessageSendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllenMessageSendingApplication.class, args);
+	}
+
+}
