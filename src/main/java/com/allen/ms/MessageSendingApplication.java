b895d96334a234f7b396fc6985f8a9d9d0371a50
luoxuetong <luoxuetong@9fbank.com.cn>
2020-05-13 13:45:05 +0800

    修改代码

--- src/main/java/com/allen/ms/MessageSendingApplication.java
+++ src/main/java/com/allen/ms/MessageSendingApplication.java
@@ -2,15 +2,16 @@ package com.allen.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
+// 要给定包路径，否则对jar里的FeignClient扫描不到
+@EnableFeignClients(basePackages = "com.allen")
+// 要给定包路径，否则对jar里的Bean扫描不到
+@ComponentScan(basePackages = "com.allen")
 public class MessageSendingApplication {
 
 	public static void main(String[] args) {
