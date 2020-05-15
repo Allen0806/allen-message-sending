82e23328713a92dec48c3ebe0abcb3a60ad91640
luoxuetong <luoxuetong@9fbank.com.cn>
2020-05-15 17:40:16 +0800

    修改代码

--- src/main/java/com/allen/ms/MessageSendingApplication.java
+++ src/main/java/com/allen/ms/MessageSendingApplication.java
@@ -2,7 +2,9 @@ package com.allen.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -10,6 +12,8 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableEurekaClient
 // 要给定包路径，否则对jar里的FeignClient扫描不到
 @EnableFeignClients(basePackages = "com.allen")
+@EnableHystrix
+@EnableCircuitBreaker
 // 要给定包路径，否则对jar里的Bean扫描不到
 @ComponentScan(basePackages = "com.allen")
 public class MessageSendingApplication {
--- src/main/java/com/allen/ms/message/controller/MessageSendController.java
+++ src/main/java/com/allen/ms/message/controller/MessageSendController.java
@@ -1,14 +1,17 @@
 package com.allen.ms.message.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.allen.mf.message.process.dto.MessageDTO;
 import com.allen.mf.message.process.feign.MessageReceiveClient;
 import com.allen.tool.result.BaseResult;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 /**
  *
@@ -20,15 +23,48 @@ import com.allen.tool.result.BaseResult;
 @RestController
 @RequestMapping("/ms")
 public class MessageSendController {
-	
+
 	@Autowired
 	private MessageReceiveClient messageReceiveClient;
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@PostMapping("/send")
-	@ResponseBody
-	public BaseResult<Object> send(){
+	public BaseResult<Object> send() {
 		MessageDTO message = new MessageDTO();
 		message.setMessageNo("202005120001");
 		return messageReceiveClient.receive(message);
+	}
+
+	/**
+	 * 验证Hystrix
+	 */
+	@SuppressWarnings("unchecked")
+	@PostMapping("/send/{messageNo}")
+	@HystrixCommand(commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+			@HystrixProperty(name = "execution.isolation.strategy", value = "THREAD") }, fallbackMethod = "sendFailure")
+	public BaseResult<Object> send(@PathVariable("messageNo") String messageNo) {
+		MessageDTO message = new MessageDTO();
+		message.setMessageNo(messageNo);
+		return restTemplate.postForObject("http://allen-message-forwarding-server/mf/process/message/receive", message,
+				BaseResult.class);
+	}
+
+	/**
+	 * 发送消息Hystrix fallback方法
+	 * 
+	 * @param messageDTO 消息对象
+	 * @return 响应对象
+	 */
+	public BaseResult<Object> sendFailure(String messageNo) {
+		BaseResult<Object> baseResult = new BaseResult<>();
+		baseResult.setStatus(BaseResult.STATUS_SYSTEM_FAILURE);
+		baseResult.setMessage("系统繁忙，请稍后再试");
+		MessageDTO message = new MessageDTO();
+		message.setMessageNo(messageNo);
+		baseResult.setData(message);
+		return baseResult;
 	}
 }
